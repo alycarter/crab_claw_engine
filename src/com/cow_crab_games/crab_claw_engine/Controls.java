437adcbd71b5142adc08f07e567093847fb4547d
Alex Carter <alycarter@hotmail.co.uk>
2013-04-02 21:15:48 +0100

    initial commit
    game loop in
    support for full screen
    support for windowed
    support for resizing the window
    support for resizing the render resolution
    separate render resolution to window resolution support
    added keyboard controls support
    added mouse controls support

---
+++ src/com/cow_crab_games/crab_claw_engine/Controls.java
@@ -0,0 +1,161 @@
+package com.cow_crab_games.crab_claw_engine;
+
+import java.awt.Canvas;
+import java.awt.MouseInfo;
+import java.awt.Point;
+import java.awt.event.KeyEvent;
+import java.awt.event.KeyListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import java.util.ArrayList;
+
+import javax.swing.SwingUtilities;
+
+
+public class Controls implements KeyListener, MouseListener{
+	
+	private ArrayList<Integer> tempKeysPressed = new ArrayList<Integer>();
+	private ArrayList<Integer> tempKeysTyped = new ArrayList<Integer>();
+	private ArrayList<Integer> keysPressed = new ArrayList<Integer>();
+	private ArrayList<Integer> keysTyped = new ArrayList<Integer>();
+	
+	private boolean leftDown =false;
+	private boolean leftClicked = false;
+	private boolean leftDownTemp =false;
+	private boolean leftClickedTemp = false;
+	private boolean rightDown =false;
+	private boolean rightClicked = false;
+	private boolean rightDownTemp =false;
+	private boolean rightClickedTemp = false;
+	public Point mouseLocation;
+	
+	private Canvas game;
+	
+	public Controls(Canvas game){
+		this.game=game;
+	}
+	
+	public void update(){
+		double lx=game.getLocationOnScreen().getX();
+		double ly=game.getLocationOnScreen().getY();
+		
+		mouseLocation = new Point((int)(MouseInfo.getPointerInfo().getLocation().getX()-lx),(int)( MouseInfo.getPointerInfo().getLocation().getY()-ly));
+		
+		this.keysPressed.clear();
+		for (int i = 0;i<tempKeysPressed.size();i++){
+			this.keysPressed.add(tempKeysPressed.get(i).intValue());
+		}
+		this.keysTyped.clear();
+		for (int i = 0;i<tempKeysTyped.size();i++){
+			this.keysTyped.add(tempKeysTyped.get(0).intValue());
+			this.tempKeysTyped.remove(0);
+		}
+		leftClicked=leftClickedTemp;
+		leftClickedTemp=false;
+		leftDown=leftDownTemp;
+		rightClicked=rightClickedTemp;
+		rightClickedTemp=false;
+		rightDown=rightDownTemp;
+	}
+	
+	public void clearControls(){
+		keysPressed.clear();
+		keysTyped.clear();
+		tempKeysPressed.clear();
+		tempKeysTyped.clear();
+		leftDown =false;
+		leftClicked = false;
+		leftDownTemp =false;
+		leftClickedTemp = false;
+		rightDown =false;
+		rightClicked = false;
+		rightDownTemp =false;
+		rightClickedTemp = false;	
+	}
+
+	@Override
+	public void keyPressed(KeyEvent e) {
+		if(!this.tempKeysPressed.contains(new Integer(e.getKeyCode()))){
+			this.tempKeysPressed.add(e.getKeyCode());
+		}
+	}
+
+	@Override
+	public void keyReleased(KeyEvent e) {
+		this.tempKeysPressed.remove(new Integer(e.getKeyCode()));
+		if(!this.tempKeysTyped.contains(new Integer(e.getKeyCode()))){
+			this.tempKeysTyped.add(e.getKeyCode());
+		}
+	}
+
+	@Override
+	public void keyTyped(KeyEvent e) {
+		
+	}
+	
+	public boolean isPressed(int key){
+		return this.keysPressed.contains(new Integer(key));
+	}
+	public boolean isTyped(int key){
+		return this.keysTyped.contains(new Integer(key));
+	}
+
+	@Override
+	public void mouseClicked(MouseEvent e) {
+		
+	}
+
+	@Override
+	public void mouseEntered(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mouseExited(MouseEvent e) {
+		// TODO Auto-generated method stub
+		
+	}
+
+	@Override
+	public void mousePressed(MouseEvent e) {
+		if(SwingUtilities.isLeftMouseButton(e)){
+			leftDownTemp=true;
+		}
+		if(SwingUtilities.isRightMouseButton(e)){
+			rightDownTemp=true;
+		}
+	}
+
+	@Override
+	public void mouseReleased(MouseEvent e) {
+		if(SwingUtilities.isLeftMouseButton(e)){
+			leftClickedTemp=true;
+			leftDownTemp=false;
+			
+		}
+		if(SwingUtilities.isRightMouseButton(e)){
+			rightClickedTemp=true;
+			rightDownTemp=false;
+			
+		}
+		
+	}
+	
+	public boolean leftMousePressed(){
+		return leftDown;
+	}
+	
+	public boolean leftMouseClicked(){
+		return leftClicked;
+	}
+	
+	public boolean rightMousePressed(){
+		return rightDown;
+	}
+	
+	public boolean rightMouseClicked(){
+		return rightClicked;
+	}
+	
+}
---
+++ src/com/cow_crab_games/crab_claw_engine/Game.java
@@ -0,0 +1,154 @@
+package com.cow_crab_games.crab_claw_engine;
+
+import java.awt.Canvas;
+import java.awt.Graphics;
+import java.awt.GraphicsDevice;
+import java.awt.GraphicsEnvironment;
+import java.awt.Toolkit;
+import java.awt.event.WindowEvent;
+import java.awt.image.BufferedImage;
+
+import javax.swing.JFrame;
+
+public abstract class Game {
+	
+	private JFrame frame;
+	private Canvas canvas;
+	
+	private final String title;
+	
+	private int windowedFrameWidth=0;
+	private int windowedFrameHeight=0;
+	
+	private BufferedImage image;
+	private int renderResolutionWidth;
+	private int renderResolutionHeight;
+	
+	private GraphicsDevice gd;
+	
+	private Controls controls;
+	
+	private boolean running=false;
+	
+	public Game(String title, int windowedFrameWidth, int windowedFrameHeight) {
+		this.title=title;
+		this.windowedFrameWidth=windowedFrameWidth;
+		this.windowedFrameHeight=windowedFrameHeight;
+		renderResolutionWidth=windowedFrameWidth;
+		renderResolutionHeight=windowedFrameHeight;
+		initialize();
+	}
+	
+	public Game(String title, int windowedFrameWidth, int windowedFrameHeight, int renderResolutionWidth, int renderResolutionHeight) {
+		this.title=title;
+		this.windowedFrameWidth=windowedFrameWidth;
+		this.windowedFrameHeight=windowedFrameHeight;
+		this.renderResolutionWidth=renderResolutionWidth;
+		this.renderResolutionHeight=renderResolutionHeight;
+		initialize();
+	}
+	
+	public void play(){
+		new Thread(){
+			public void run() {
+				frame.setVisible(true);
+				canvas.requestFocusInWindow();
+				running = true;
+//				long ns = 1000000000;
+//				long start=0;
+//				long end = 0;
+				while(running){
+					update();
+					render();
+				}
+				gd.setFullScreenWindow(null);
+				Toolkit.getDefaultToolkit().getSystemEventQueue().postEvent(new WindowEvent(frame,WindowEvent.WINDOW_CLOSING));
+			}
+		}.start();
+	}
+	
+	private void render() {
+		if(canvas.getBufferStrategy()==null){
+			canvas.createBufferStrategy(3);
+		}
+		onRender(image.getGraphics());
+		canvas.getBufferStrategy().getDrawGraphics().drawImage(image, 0, 0, canvas.getWidth(), canvas.getHeight(),null);
+		canvas.getBufferStrategy().show();
+	}
+
+	private void update() {
+		controls.update();
+		onUpdate();
+	}
+
+	public abstract void onUpdate();
+	
+	public abstract void onRender(Graphics g);
+
+	private void initialize(){
+		gd = GraphicsEnvironment.getLocalGraphicsEnvironment().getDefaultScreenDevice();
+		frame= new JFrame(title); 
+		canvas= new Canvas();
+		frame.getContentPane().add(canvas);
+		setWindowedFrameSize(windowedFrameWidth, windowedFrameHeight);
+		image= new BufferedImage(renderResolutionWidth,renderResolutionHeight,BufferedImage.TYPE_INT_ARGB);
+		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		controls = new Controls(canvas);
+		canvas.addKeyListener(controls);
+		canvas.addMouseListener(controls);
+		onInitialize();
+	}
+	
+	public void setWindowedFrameSize(int width, int height){
+		this.windowedFrameWidth=width;
+		this.windowedFrameHeight=height;
+		frame.setSize(width, height);
+		canvas.setSize(width, height);
+	}
+	
+	public void setRenderResolutionSize(int width, int height){
+		this.renderResolutionWidth=width;
+		this.renderResolutionHeight=height;
+		image= new BufferedImage(renderResolutionWidth,renderResolutionHeight,BufferedImage.TYPE_INT_ARGB);
+	}
+	
+	public int getResolutionWidth(){
+		return renderResolutionWidth;
+	}
+	
+	public int getResolutionHeight(){
+		return renderResolutionHeight;
+	}
+	
+	public int getWindowedFrameWidth(){
+		return windowedFrameWidth;
+	}
+	
+	public int getWindowedFrameHeight(){
+		return windowedFrameHeight;
+	}
+	
+	public void endGame(){
+		running=false;
+	}
+	
+	public void setFullScreen(){
+		frame.setUndecorated(true);
+		gd.setFullScreenWindow(frame);
+		canvas.setSize(frame.getContentPane().getSize());
+	}
+	
+	public void setWindowedMode(){
+		frame.setUndecorated(false);
+		setWindowedFrameSize(windowedFrameWidth, windowedFrameHeight);
+		gd.setFullScreenWindow(null);
+	}
+	
+	public Controls getControls(){
+		return controls;
+	}
+	
+	public abstract void onInitialize();
+	
+	
+}
